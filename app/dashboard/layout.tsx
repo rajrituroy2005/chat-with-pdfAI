34490aa26d3fc24b160d10bbe663f05b0deb172c
Rituraj Roy <riturajroy2927@gmail.com>
2026-01-06 21:32:34 +0530

    DAY-02 WORK

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,16 @@
+import Documents from "@/components/Documents";
+import Header from "@/components/Header";
+import { ClerkLoaded } from "@clerk/nextjs";
+
+function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <ClerkLoaded>
+      <div>
+        <Header />
+        {children}
+      </div>
+    </ClerkLoaded>
+  );
+}
+
+export default DashboardLayout;
---
+++ components/FileUploader.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { useCallback, useEffect } from "react";
+import { useDropzone } from "react-dropzone";
+import {
+  CheckCircleIcon,
+  CircleArrowDown,
+  HammerIcon,
+  RocketIcon,
+  SaveIcon,
+} from "lucide-react";
+// import useUpload, { StatusText } from "@/hooks/useUpload";
+import { useRouter } from "next/navigation";
+// import useSubscription from "@/hooks/useSubscription";
+// import { useToast } from "./ui/use-toast";
+
+function FileUploader() {
+  const onDrop = useCallback(
+    async (acceptedFiles: File[]) => {
+      // Do something with the files
+      console.log(acceptedFiles);
+},[])
+  
+  const { getRootProps, getInputProps, isDragActive, isFocused, isDragAccept } =
+    useDropzone({
+      onDrop,
+    });
+
+ return (
+    <div className="flex flex-col gap-4 items-center max-w-7xl mx-auto">
+      {/* Loading... tomorrow! */}
+
+      <div
+        {...getRootProps()}
+        className={`p-10 border-2 border-dashed mt-10 w-[90%] border-indigo-600 text-indigo-600 rounded-lg h-96 flex items-center justify-center`}
+      >
+        <input {...getInputProps()} />
+
+       <div className="flex flex-col items-center justify-center">
+    {isDragActive ? (
+        <>
+        <RocketIcon className="h-20 w-20 animate-ping" />
+        <p>Drop the files here ...</p>
+        </>
+    ) : (
+        <>
+        <CircleArrowDown className="h-20 w-20 animate-bounce" />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+        </>
+    )}
+    </div>
+        </div>
+    </div>
+  );
+}
+
+export default FileUploader;
