e148641055f24afc6cc548de60bb34031100039b
Rituraj Roy <riturajroy2927@gmail.com>
2026-01-07 16:08:53 +0530

    vercel error fixed

---
+++ hooks/useUpload.ts
@@ -0,0 +1,20 @@
+"use client";
+
+import { useState } from "react";
+
+export enum StatusText {
+  UPLOADING = "Uploading file...",
+  UPLOADED = "File uploaded successfully",
+  SAVING = "Saving file to database...",
+  GENERATING = "Generating AI Embeddings, This will only take a few seconds...",
+}
+
+export type Status = StatusText[keyof StatusText];
+
+function useUpload() {
+  const [progress, setProgress] = useState<number | null>(null);
+  const [fileId, setFileId] = useState<string | null>(null);
+  const [status, setStatus] = useState<Status | null>(null);
+}
+
+export default useUpload;
