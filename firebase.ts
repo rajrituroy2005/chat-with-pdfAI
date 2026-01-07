edb4059be6ee8f22c58457f59250986c2ceafe1d
Rituraj Roy <riturajroy2927@gmail.com>
2026-01-07 12:51:18 +0530

    slight change

--- firebase.ts
+++ firebase.ts
@@ -14,6 +14,6 @@ const firebaseConfig = {
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
 const db = getFirestore(app);
-const storage = getFirestore(app);
+const storage = getStorage(app);
 
 export {db,storage};
