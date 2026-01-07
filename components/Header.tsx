9f09092296c018dee74b5099cc733a81048cc9c3
Rituraj Roy <riturajroy2927@gmail.com>
2026-01-07 12:31:55 +0530

    Added a upload File UI

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,17 @@
+"use client";
+
 import { SignedIn, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import { Button } from "./ui/button";
-import { FilePlus2 } from "lucide-react";
+import { FilePlus } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 function Header() {
+  const router = useRouter();
+  const handleClick = () => {
+    // Check if user is FREE tier and if theyre over the file limit, push to the upgrade page
+    router.push("/dashboard/upload");
+  };
   return (
     <div className="flex justify-between bg-white shadow-sm p-5 border-b">
       <Link href="/dashboard" className="text-2xl">
@@ -19,8 +27,11 @@ function Header() {
           <Button asChild variant="outline">
             <Link href="/dashboard">My Documents</Link>
           </Button>
-
-          {/* Upgrade Button */}
+          
+          <Button onClick={handleClick} className="bg-indigo-500 scale-100 cursor-pointer mx-2 my-auto hover:scale-98" > 
+            <FilePlus />
+          </Button>
+          
           <UserButton />
         </div>
       </SignedIn>
